e4bfe745250c5ad77ae35086bdbec36f57f5012e
Guillermo Rauch <rauchg@gmail.com>
2012-10-04 21:13:36 -0700

    index: initial module

---
+++ index.js
@@ -0,0 +1,133 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Socket = require('engine.io-client').Socket
+  , Document = require('./document')
+  , debug = require('debug')('mydb-client')
+  , Emitter, json;
+
+try {
+  Emitter = require('emitter');
+  json = require('json');
+} catch(e) {
+  Emitter = require('emitter-component');
+  json = require('json-component');
+}
+
+/**
+ * Module exports.
+ */
+
+module.exports = Manager;
+
+/**
+ * Manager constructor.
+ *
+ * @param {String|Object} optional, url to connect socket to or eio opts
+ * @api public
+ */
+
+function Manager(url, opts){
+  var self = this;
+  this.socket = new Socket(url);
+  this.socket.onmessage = function(msg){
+    self.onMessage(msg);
+  };
+  this.socket.onclose = function(){
+    self.emit('disconnect');
+  };
+}
+
+/**
+ * Mixes in `Emitter`.
+ */
+
+Emitter(Manager.prototype);
+
+/**
+ * Called when a message is received.
+ *
+ * @api private
+ */
+
+Manager.prototype.onMessage = function(msg){
+  var obj = json.parse(msg);
+  var sid = obj.i;
+  var doc = this.subscriptions[sid];
+
+  if (!doc && obj.data) {
+    debug('ignoring data for inexisting subscription %s', sid);
+    return;
+  }
+
+  switch (obj.e) {
+    case 'p': // payload
+      doc.$payload(obj.data);
+      break;
+
+    case 'o': // operation
+      doc.$op(obj.data);
+      break;
+
+    case 'u': // unsubscribe confirmation
+      this.emit('unsubscribe', sid);
+      break;
+  }
+};
+
+/**
+ * Subscribes to the given sid.
+ *
+ * @param {String} id
+ * @api private
+ */
+
+Manager.prototype.subscribe = function(id, doc){
+  this.subscriptions[id] = doc;
+  this.write({ e: 'subscribe', id: id });
+  this.emit('subscribe', id);
+};
+
+/**
+ * Writes the given object to the socket.
+ *
+ * @api private
+ */
+
+Manager.prototype.write = function(obj){
+  this.socket.write(json.stringify(obj));
+};
+
+/**
+ * Destroys a subscription.
+ *
+ * @param {String} subscription id
+ * @api private
+ */
+
+Manager.prototype.unsubscribe = function(id){
+  var sub = this.subscriptions[id];
+
+  if (!sub) {
+    throw new Error('Trying to destroy inexisting subscription: ' + id);
+  }
+
+  delete this.subscriptions[id];
+  this.write({ e: 'unsubscribe', id: id });
+  this.emit('destroy', sub);
+};
+
+/**
+ * Retrieves a document.
+ *
+ * @return {Document}
+ * @api public
+ */
+
+Manager.prototype.get = function(url){
+  var doc = new Document(this);
+  if (url) doc.load(url);
+  return doc;
+};
